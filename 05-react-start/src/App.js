354ff17d7bf78425b45757bbdec8159a612afd61
Pritam8911 <ls.pritam1891@gmail.com>
2022-06-04 17:31:26 +0600

    custom component 2 added

--- 05-react-start/src/App.js
+++ 05-react-start/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import CustomComponent from "./components/00-custom-components"
+import CustomComponent2 from "./components/01-custom-components-2"
 
 function App() {
   return (
     <div>
         <CustomComponent/>
+        <CustomComponent2/>
     </div>
   )
 }
---
+++ 05-react-start/src/components/01-custom-components-2/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' width="40px" />
+        </nav>
+      </header>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+      <footer>
+        <small>© 2022 Rayhan development. All rights reserved.</small>
+      </footer>
+    </div>
+  )
+}
+
+export default index
