4d3bbaddc1683bc744384cebe15f68383c1e9020
Pritam8911 <ls.pritam1891@gmail.com>
2022-06-04 17:09:16 +0600

    react start from here

---
+++ 05-react-start/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CustomComponent from "./components/00-custom-components"
+
+function App() {
+  return (
+    <div>
+        <CustomComponent/>
+    </div>
+  )
+}
+
+export default App
---
+++ 05-react-start/src/components/00-custom-components/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+        <ol>
+            <li>I Love Programming</li>
+            <li>I Love React</li>
+            <li>React is very cool</li>
+            <li>React is very easy</li>
+        </ol>
+    </div>
+  )
+}
+
+export default index
