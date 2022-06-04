5a2c007b45b08e7cd65db3b6b6dde2029a148562
Pritam8911 <ls.pritam1891@gmail.com>
2022-06-04 16:30:49 +0600

    first project added

---
+++ 04-project-part1-markup/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' height="40px"/>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+  )
+}
+
+export default App
