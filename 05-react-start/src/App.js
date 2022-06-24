415a82734b408427b5c25acf99e555579eed6ef4
rayhan <prog.rayhan1@gmail.com>
2022-06-24 16:53:41 +0600

    styling with css added

--- 05-react-start/src/App.js
+++ 05-react-start/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import CustomComponent from "./components/00-custom-components"
 import CustomComponent2 from "./components/01-custom-components-2"
 import ParentChildComponent from "./components/02-Parent-Child-Components"
+import StylingWithCss from './components/03-Styling-with-CSS'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <CustomComponent />
       <CustomComponent2 />
       <ParentChildComponent />
+      <StylingWithCss/>
     </div>
   )
 }
---
+++ 05-react-start/src/components/03-Styling-with-CSS/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './style.css'
+
+function Header() {
+    return (
+        <header>
+            <nav className='nav'>
+                <img src='https://th.bing.com/th/id/R.f81a6f373c244b1f70f4b7402b5ab372?rik=rbXh4ieLuKt%2bmA&riu=http%3a%2f%2flogos-download.com%2fwp-content%2fuploads%2f2016%2f09%2fReact_logo_logotype_emblem.png&ehk=QhGOkKcUKCU7FBQgHOajOiJqJBACUTD2Ni6LsfqzCEA%3d&risl=&pid=ImgRaw&r=0' className='nav-logo' />
+                <ul className='nav-items'>
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function index() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default index
