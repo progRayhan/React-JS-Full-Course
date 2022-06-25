fe72333557aff878cf815186aa734b8f4894f8f0
rayhan <prog.rayhan1@gmail.com>
2022-06-25 12:46:32 +0600

    images with cra added

--- 05-react-start/src/App.js
+++ 05-react-start/src/App.js
@@ -4,6 +4,7 @@ import CustomComponent2 from "./components/01-custom-components-2"
 import ParentChildComponent from "./components/02-Parent-Child-Components"
 import StylingWithCss from './components/03-Styling-with-CSS'
 import OrganizeComponent from "./components/04-Organize-Component/Parent"
+import ImageWithCRA from './components/05-Images-with-CRA'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <ParentChildComponent />
       <StylingWithCss/>
       <OrganizeComponent/>
+      <ImageWithCRA/>
     </div>
   )
 }
---
+++ 05-react-start/src/components/05-Images-with-CRA/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Car from './images/car.jpg'
+
+function index() {
+  return (
+    <div>
+        <img
+        src={Car}
+        alt='Car'
+        width='200px'/>
+    </div>
+  )
+}
+
+export default index
